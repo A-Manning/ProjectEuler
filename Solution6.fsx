b30f1f9722bb1005cdba087a8c9dd414075bc5ec
A-Manning <ashermanning@yahoo.co.uk>
2015-01-28 00:31:24 -0500

    Problem 6 solution in F#
    Again, solved before Java. This was a brilliant example of just how
    great F# can be for certain problems.  7 lines of code , efficient,
    readable, and general.

---
+++ Solution6.fsx
@@ -0,0 +1,18 @@
+﻿(*
+
+A Manning
+
+Learn more about F# at http://fsharp.net
+
+This solution is really easy in F#, and really efficient too. 
+
+*)
+
+let square x = x*x
+let listTo x = [1..x]
+let squares l= l |> List.map square
+let sumOfSquares x = listTo x |> squares |> List.sum
+let squareOfSum x = listTo x |> List.sum |> square
+let answer x = squareOfSum x - sumOfSquares x
+
+answer 100
