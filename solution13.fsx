402ab8c19ef7559a99bff3a3f3489664140216d9
A-Manning <ashermanning@yahoo.co.uk>
2015-02-01 04:43:25 -0500

    Problem 13 solution in F#
    Beautiful solution - only 3 lines! No efficiency issues there.

---
+++ solution13.fsx
@@ -0,0 +1,18 @@
+﻿(*
+
+A Manning
+
+Learn more about F# at http://fsharp.net
+
+Fast, clean, only 3 lines. Great success. 
+The problem is stored in a text file called problem13.txt. 
+
+*)
+open System.IO
+open System.Numerics
+
+//ls is a sequence of lines of the file
+let ls = seq { yield! File.ReadLines(@"C:\Users\A Manning\Documents\Work\Project Euler\Problem 13\problem13.txt") }
+let map x = BigInteger.Parse(ls |> Seq.nth (x-1)) // map x gives the xth line of the file as a BigInteger
+let ans = List.map map [1..100] |> List.sum // Sums the numbers , gives the answer
+
