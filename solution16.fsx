b1c54b8b3b703971c6b64acf41770a2310ba9fa9
A-Manning <ashermanning@yahoo.co.uk>
2015-03-03 10:33:47 -0500

    F# solution

---
+++ solution16.fsx
@@ -0,0 +1,10 @@
+﻿(*
+
+A Manning
+
+Only two lines of code...really shows the power of F#. 
+
+*)
+open System.Numerics
+let s = string (BigInteger.Pow((BigInteger 2),1000)) // s is the string value of 2^1000
+s.ToCharArray() |> Array.map(fun x -> (int x)-(48)) |> Array.sum // makes s a char array, changes char vals to integers, sums them
