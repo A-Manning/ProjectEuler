6952e8a0eb456ed70a8612f86670a251e599a797
A-Manning <ashermanning@yahoo.co.uk>
2015-03-03 10:37:55 -0500

    F# solution

---
+++ solution20.fsx
@@ -0,0 +1,10 @@
+﻿open System.Numerics
+let rec factorial (x:BigInteger) = 
+        if (x = bigint 2)
+            then x
+        else x * (factorial (x- bigint 1))
+
+let fact100 = factorial (bigint 100)
+fact100.ToString().ToCharArray() 
+|> Array.map(fun x -> (int(x) - 48))     
+|> Array.sum                                                                                                                                                                                                                                                     
