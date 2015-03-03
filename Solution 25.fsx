a65258755af595e19e1dffe12916deab13f88da9
A-Manning <ashermanning@yahoo.co.uk>
2015-03-03 10:48:48 -0500

    F# solution

---
+++ Solution 25.fsx
@@ -0,0 +1,10 @@
+﻿//Tail Recursion is Fast! This took 0.093 secs. 
+open System.Numerics
+let fibNDigits N = 
+    let rec find (m:BigInteger) (x:BigInteger) (i:int) = 
+        if x.ToString().Length = N
+            then i
+        else find x (m+x) (i+1)
+    find (bigint 1) (bigint 1) 2
+
+fibNDigits 1000
