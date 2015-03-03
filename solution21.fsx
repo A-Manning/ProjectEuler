3e73a7c0cde5d938733db1d6568d176e12502e3b
A-Manning <ashermanning@yahoo.co.uk>
2015-03-03 10:41:58 -0500

    Revert "F# solution"
    This reverts commit 20528c71950588d7812bbe0ebc08dee9a764abec.

--- solution21.fsx
+++
@@ -1,8 +0,0 @@
-﻿let divisorsum a = List.filter(fun x -> a%x = 0 )[1..(a-1)] |> List.sum
-divisorsum 220
-let isamicable a = 
-    let tmp1 = divisorsum a
-    let tmp2 = divisorsum (tmp1)
-    a = tmp2 && a <> tmp1
-
-List.filter(fun x -> isamicable x) [1..10000] |> List.sum
